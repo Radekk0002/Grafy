b17acc7b6750792170be9d30cdb2ac8de4562061
Radek <radekk0002@gmail.com>
2021-06-12 19:30:29 +0200

    Add algorithm for finding shortest cycle defined by nodes

--- script.js
+++ script.js
@@ -2,12 +2,14 @@ const matrixEl = document.querySelector("#matrix");
 const matrixElCopy = matrixEl.cloneNode(true);
 const resetButton = document.querySelector("#reset");
 const findCycleButton = document.querySelector("#findCycle");
+const findCycleButton2 = document.querySelector("#findCycle2");
 const addDimensionButton = document.querySelector("#addDimension");
 const removeDimensionButton = document.querySelector("#removeDimension");
 const weightEl = document.querySelector("#weight");
 const CycleEl = document.querySelector("#cycle");
 let matrix = [];
 
+// METODY POBIERAJĄCE I PRZYGOTOWUJĄCE GRAF Z MACIERZY
 function reset() {
     matrixEl.innerHTML = "";
     for (let i = 0; i < matrixElCopy.children.length; i++) {
@@ -22,13 +24,23 @@ function reset() {
 function AddDimension() {
     const matrixChildren = matrixEl.children;
 
+    let row = document.createElement("div");
+    row.setAttribute("class", "matrixRow");
+
     Array.from(matrixChildren).forEach((el) => {
         const input = document.createElement("input");
         input.setAttribute("type", "text");
         el.appendChild(input);
+
+        const inp = document.createElement("input");
+        inp.setAttribute("type", "text");
+        row.appendChild(inp);
     });
 
-    const row = matrixChildren[0].cloneNode(true);
+    const inp = document.createElement("input");
+    inp.setAttribute("type", "text");
+    row.appendChild(inp);
+
     matrixEl.appendChild(row);
 }
 
@@ -74,22 +86,175 @@ removeDimensionButton.addEventListener("click", () => {
     removeDimension();
 });
 
+// DLA ALGORYTMU 1
 findCycleButton.addEventListener("click", () => {
     convertMatrix();
-    FindCycle();
+    FindCycle1();
+});
+
+// Dla ALGORYTMU 2
+findCycleButton2.addEventListener("click", () => {
+    convertMatrix();
+    FindCycle2();
 });
 
 //
+//  ALGORYTM 1 - WYSZUKUJE PO ILOŚCI WIERZCHOŁKÓW
 //
-// ALGORYTM ALGORYTM ALGORYTM
 
-function FindCycle() {
+function FindCycle1() {
+    pre = Array.from({ length: matrix.length }, (i) => (i = -1));
+    post = Array.from({ length: matrix.length }, (i) => (i = -1));
+
+    DFS(matrix, pre, post);
+    console.log(pre);
+    console.log(post);
+    // KRAWĘDZIE POWROTNE
+    backEdges = FindBackEdges(pre, post, matrix);
+    console.log(backEdges);
+
+    // ŚCIEŻKI DO PODANYCH WIERZCHOŁKÓW Z KRAWĘDZI POWROTNYCH
+    paths = [];
+
+    for (let i = 0; i < backEdges.length; i++) {
+        start = backEdges[i][1];
+        end = backEdges[i][0];
+        // URUCHOMIENIE BFS DLA KAŻDEJ KRAWĘDZI POWROTNEJ
+        path = FindShortestPath(start, end, matrix);
+
+        v = end;
+        // ZMIENNA PRZETRZYMUJĄCA UPORZĄDKOWANĄ DROGĘ
+        transformedPath = [];
+        // ŚCIEŻKĘ TRZEBA ODCZYTYWAĆ WSPAK
+        while (v > -1) {
+            transformedPath.unshift(v);
+            v = path[v];
+        }
+        paths.push(transformedPath);
+    }
+    console.log(paths);
+    shortestCycle = null;
+    if (paths.length > 0) {
+        // ZNALEZIENIE NAJKRÓTSZEGO CYKLU PORÓWNUJĄC DŁUGOŚCI TABLIC ŚCIEŻEK
+        shortestCycle = paths.reduce((prev, next) =>
+            prev.length > next.length ? next : prev
+        );
+    }
+    console.log(shortestCycle);
+
+    // WYŚWIETLENIE WYNIKU
+    nodesCount = 0;
+    cycle = "brak";
+
+    if (shortestCycle != null) {
+        nodesCount = shortestCycle.length;
+        cycle = shortestCycle.join("-");
+    }
+    weightEl.innerHTML = `Ilość wierzchołków: ${nodesCount}`;
+    CycleEl.innerHTML = `Cykl: ${cycle}${
+        shortestCycle != null ? `-${shortestCycle[0]}` : ""
+    }`;
+}
+
+function DFS(matrix, pre, post) {
+    // INICJALIZACJA TABLICY ODWIEDZONE
+    visited = Array.from({ length: matrix.length }, (i) => (i = false));
+    licznik = 1;
+
+    for (let i = 0; i < matrix.length; i++) {
+        if (!visited[i]) Explore(i, pre, post);
+    }
+
+    function Explore(v, pre, post) {
+        visited[v] = true;
+
+        pre[v] = licznik;
+        licznik += 1;
+
+        for (let i = 0; i < matrix[v].length; i++) {
+            if (!visited[i] && matrix[v][i] != 0) Explore(i, pre, post);
+        }
+        post[v] = licznik;
+        licznik += 1;
+    }
+}
+
+// ALGORYTM ZNAJDUJE KRAWĘDZIE POWROTNE (OPARTY O ALGORYTM BFS)
+function FindBackEdges(pre, post, matrix) {
+    // INICJALIZACJA TABLICY ODWIEDZONE
+    visited = Array.from({ length: matrix.length }, (i) => (i = false));
+    // TABLICA KRAWĘDZI POWROTNYCH
+    backEdges = [];
+    queue = [];
+    queue.push(0);
+    visited[0] = true;
+
+    while (queue.length != 0) {
+        v = queue.shift();
+        for (let i = 0; i < matrix[v].length; i++) {
+            if (!visited[i] && matrix[v][i] != 0) {
+                queue.push(i);
+                visited[i] = true;
+            }
+            // SPRAWDZENIE CZY JEST KRAWĘDZIĄ POWROTNĄ
+            if (
+                matrix[v][i] != 0 &&
+                pre[i] < pre[v] &&
+                pre[v] < post[v] &&
+                post[v] < post[i]
+            ) {
+                tmp = [v, i];
+                backEdges.push(tmp);
+            }
+        }
+    }
+    return backEdges;
+}
+
+// ALGORYTM ZNAJDUJE NAJKRÓTSZĄ ŚCIEŻKĘ POMIĘDZY PODANYMI WIERZCHOŁKAMI - OPARTY O BFS
+function FindShortestPath(start, end, matrix) {
+    // INICJALIZACJA TABLICY ODWIEDZONE
+    visited = Array.from({ length: matrix.length }, (i) => (i = false));
+    // INICJALIZACJA ŚCIEŻKI POWROTNEJ - ELEMENT POD DANYM INDEKSEM WSKAZUJE NA WIERZCHOŁEK Z KTÓREGO DOJDZIEMY NA PODANY INDEKS(WIERZCHOŁEK)
+    path = Array.from({ length: matrix.length }, (i) => (i = -1));
+    queue = [];
+    queue.push(start);
+    visited[start] = true;
+
+    while (queue.length != 0) {
+        v = queue.shift();
+        if (v != end) {
+            for (let i = 0; i < matrix[v].length; i++) {
+                if (!visited[i] && matrix[v][i] != 0) {
+                    path[i] = v;
+                    queue.push(i);
+                    visited[i] = true;
+                }
+            }
+        } else {
+            break;
+        }
+    }
+    return path;
+}
+
+//
+//  ALGORYTM 2 - WYSZUKUJE PO SUMIE WAG
+//
+
+function FindCycle2() {
     // CZĘŚĆ I ALGORYTMU
 
+    if (matrix.length == 1) {
+        alert("Dodaj kolejne wierzchołki");
+        return;
+    }
+
+    // CZĘŚĆ I ALGORYTMU
     var start = 0;
     MacierzAlgorytmiczna = new Array(matrix.length);
 
-    // Generuję (będzie) dwu-wymiarową tablicę gdzie będą wszystkie elementy cyklu
+    // Generuję (będzie) trój-wymiarową tablicę gdzie będą wszystkie elementy cyklu
     var ElementyDrogi = new Array(matrix.length);
     for (i = 0; i < ElementyDrogi.length; i++) {
         ElementyDrogi[i] = new Array(matrix.length);
@@ -104,7 +269,6 @@ function FindCycle() {
         Poprzednicy = [];
         Macierz = [];
         S = [];
-
         if (Dlugosci.length > 0) Dlugosci = Dlugosci.fill(0);
         else var Dlugosci = new Array(matrix.length);
         if (Poprzednicy.length > 0) Poprzednicy = Poprzednicy.fill(0);
@@ -126,7 +290,6 @@ function FindCycle() {
                 }
             }
         }
-
         var Oryginalna_Dl = Macierz.length;
         var Minimalny = 0;
         var w = 0;
@@ -222,7 +385,7 @@ function FindCycle() {
         var Min = Number.MAX_VALUE;
         Length = 0;
 
-        // szukam mina
+        // szukam min
         for (j = 0; j < MacierzAlgorytmiczna[start].length; j++) {
             if (MacierzAlgorytmiczna[start][j] != 0) {
                 Length =
@@ -237,16 +400,8 @@ function FindCycle() {
         }
         start++;
     }
-
-    if (
-        ElementyDrogi[pamY][pamX].length == 0 &&
-        ElementyDrogi[pamX][pamY].length == 0
-    ) {
-        alert("Brak cyklu");
-        return;
-    }
-
-    document.getElementById("weight").innerHTML = "Waga: " + mini;
+    document.getElementById("weight").innerHTML =
+        "Waga: " + `${mini == Number.MAX_VALUE ? 0 : mini}`;
     document.getElementById("cycle").innerHTML = "Cykl: ";
     for (i = 0; i < ElementyDrogi[pamY][pamX].length; i++) {
         document.getElementById("cycle").innerHTML +=
